207fc6ddadf4fb49b98d4c3c736676cb2f88be6a
shoeone96 <85065626+shoeone96@users.noreply.github.com>
2021-05-31 17:15:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,34 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 
-# HTML 화면
+# HTML 화면 보여주기
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
 # API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def GET_function():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': 'GET 연결되었습니다!'})
+@app.route('/list', methods=['GET'])
+def saved_contents():
+    saved_content = list(db.screw.find({}, {'_id': False}))
+    return jsonify({'all_saved_content': saved_content})
 
 
-@app.route('/api/like', methods=['POST'])
-def POST_function():
+@app.route('/save', methods=['POST'])
+def write_review():
+    content_receive = request.form['content_give']
+    doc = {
+        'content': content_receive,
+    }
+    db.screw.insert_one(doc)
+    return jsonify({'msg': '저장완료'})
+
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
     sample_receive = request.form['sample_give']
     print(sample_receive)
-    return jsonify({'msg': 'POST 연결되었습니다!'})
+    return jsonify({'msg': 'delete 연결되었습니다!'})
 
 
 if __name__ == '__main__':
